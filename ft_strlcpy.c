63741a2918adeec28a95b64d233fe7a889954e99
Janne Hyokki <jhyokki@c1r1p1.hive.fi>
2024-10-31 08:45:09 +0200

    refactor ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,23 +6,20 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 07:26:47 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/10/31 08:18:29 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/10/31 08:35:16 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	const char	*s;
-	size_t		n;
 
 	s = src;
-	n = size;
 	if (size)
 	{
-		while (*s && --n)
+		while (*s && --size)
 			*dst++ = *s++;
-		if (n == 0)
-			*dst = '\0';
+		*dst = '\0';
 	}
 	while (*s++)
 		;
