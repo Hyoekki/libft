7c4e20cce3cd635612e45660e5c3e37c016c02da
Janne Hyokki <jhyokki@c1r1p1.hive.fi>
2024-10-31 10:20:51 +0200

    fix function name for ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,11 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:45:42 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/10/31 10:15:36 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/10/31 10:19:57 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *strrchr(const char *s, int c)
+char *ft_strrchr(const char *s, int c)
 {
 	char *last;
 
