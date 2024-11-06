95e7776a0815be4b556d527185d727f79301c478
Janne Hyokki <jhyokki@c2r6p12.hive.fi>
2024-11-06 10:48:47 +0200

    add documentation to putchar

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,10 +6,16 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:27:59 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/11/04 20:28:51 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/11/06 10:47:49 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Takes character c and writes it into filedescriptor fd
+0 for stdin
+1 for stdout
+2 for stderr
+ */
 #include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd)
