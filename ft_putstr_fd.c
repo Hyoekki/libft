c923a895f70e389003847a3080fd3cfcace19f43
Janne Hyokki <jhyokki@c2r6p12.hive.fi>
2024-11-06 11:17:19 +0200

    add documentation to putstr

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,10 +6,16 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:30:43 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/11/04 20:30:58 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/11/06 11:15:42 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Takes character string s and prints it into filesdescriptor fd
+0 for stdin
+1 for stdout
+2 for stderr
+ */
 #include <unistd.h>
 #include "libft.h"
 
