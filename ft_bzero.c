4cf3c698558aad1f85db138c405308a9cdfad269
Janne Hyokki <jhyokki@c2r6p12.hive.fi>
2024-11-06 09:29:30 +0200

    add documentation to bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,10 +6,15 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 08:42:59 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/10/31 16:59:18 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/11/06 09:29:17 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Erase n bytes of the memory starting from pointer s.
+Set pointer p equal to s. While n is not equal to zero, erase the memory at p
+and decrement n.
+ */
 #include <stddef.h>
 
 void	ft_bzero(void *s, size_t n)
