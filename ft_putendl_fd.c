9bf6a5d334365f6d1f56c25ea806ef2178431b19
Janne Hyokki <jhyokki@c2r6p12.hive.fi>
2024-11-06 10:50:27 +0200

    add documentation to putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,10 +6,17 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:33:45 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/11/05 09:20:56 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/11/06 10:49:59 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Takes character string s and writes it into filedescriptor fd
+followed by newline.
+0 for stdin
+1 for stdout
+2 for stderr
+ */
 #include <unistd.h>
 #include "libft.h"
 
