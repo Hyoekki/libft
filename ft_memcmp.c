0ae0cefb346bce346a70709d4f0de814ef8afa26
Janne Hyokki <jhyokki@c2r6p12.hive.fi>
2024-11-06 10:29:55 +0200

    add documentation to memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,10 +6,14 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 11:58:46 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/11/05 12:05:43 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/11/06 10:28:37 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Compare n number of bytes of memory blocks s1 and s2. Return positive, zero or
+negative integer if they are equal or differ.
+ */
 #include <stddef.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
