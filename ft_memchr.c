54fe0836b7158cf40ade760bcc77963d3a8bd046
Janne Hyokki <jhyokki@c2r6p12.hive.fi>
2024-11-06 10:23:27 +0200

    add documentation to memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,22 +6,25 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 05:41:57 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/11/05 08:47:30 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/11/06 10:17:23 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Takes pointer s and searches first instance of c for the n number of bytes.
+Set variable pointer p equal to s amd while n is not equal to zero, iterate
+over the memory and compare p to c. Return pointer p if found and NULL if not.
+ */
 #include <stddef.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char	*p;
-	unsigned char		t;
 
 	p = s;
-	t = (unsigned char )c;
 	while (n--)
 	{
-		if (*p == t)
+		if (*p == (unsigned char)c)
 			return ((void *)p);
 		p++;
 	}
