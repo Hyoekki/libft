ebf11cbdbedf0e09d971ee89ee56e2540b3833e5
Janne Hyokki <jhyokki@c2r6p12.hive.fi>
2024-11-06 10:36:35 +0200

    add documentation to memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,10 +6,14 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:28:20 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/10/31 11:23:26 by jhyokki          ###   ########.fr       */
+/*   Updated: 2024/11/06 10:35:57 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Copies n number of bytes from memory pointed by pointer src to memory pointed by
+dest. The src is first copied into temp array and after that to dest.
+ */
 #include <stddef.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
