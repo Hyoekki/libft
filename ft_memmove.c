07408cfaacf2902c82cb6d01e234a782779d5491
Janne Hyokki <62105619+Hyoekki@users.noreply.github.com>
2025-01-13 15:18:28 +0200

    Update ft_memmove.c
    Remove non mandatory check if d == s

--- ft_memmove.c
+++ ft_memmove.c
@@ -23,14 +23,12 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-	if (d == s)
-		return (dest);
 	if (d < s)
 	{
 		while (n--)
 			*d++ = *s++;
 	}
-	else
+	else if (d != s)
 	{
 		d += n;
 		s += n;
